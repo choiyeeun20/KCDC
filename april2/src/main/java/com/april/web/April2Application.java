0cb789bc82fbeb2b2be16510af1042bdcd5dee6f
Your Name <68rhaehfdlvn@gmail.com>
2020-04-29 13:05:02 +0900

    first commit

---
+++ april2/src/main/java/com/april/web/April2Application.java
@@ -0,0 +1,13 @@
+package com.april.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class April2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(April2Application.class, args);
+	}
+
+}
