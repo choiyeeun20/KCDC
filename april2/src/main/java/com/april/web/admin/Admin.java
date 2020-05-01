38d81dc3ba4482603c87225f74254510208c10f6
Your Name <68rhaehfdlvn@gmail.com>
2020-05-01 14:49:44 +0900

    14:49

--- april2/src/main/java/com/april/web/admin/Admin.java
+++ april2/src/main/java/com/april/web/admin/Admin.java
@@ -1,7 +1,9 @@
 package com.april.web.admin;
+import org.springframework.stereotype.Component;
+
 import lombok.Getter;
 import lombok.Setter;
-
+@Component
 @Getter @Setter
 public class Admin {
 	private String  employNumber, passwd ,name , 
@@ -13,5 +15,5 @@ public class Admin {
 		return String.format("%s,%s,%s,%s,%s,%s,%s,%s", 
 				employNumber,passwd,name,position,profile,email,phoneNumber,registerDate);
 	}
-	/////
+	///////
 }
